6af4cca5c45649f1b1a72cab1ce79e47e2a1b72a
Ho Joong Daniel Kim <hjdkim@gmaill.com>
2022-06-14 01:26:11 -0300

    pool repartidor

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Logins from "./components/Logins";
 import Inicio from "./components/Inicio";
-import React from 'react'
+import React from "react";
+import PoolRepartidor from "./components/PoolRepartidor";
 
 function App() {
   return (
     <div className="container-fluid">
-
       <BrowserRouter>
         <Navbar></Navbar>
 
         <Routes>
-
-          <Route index element={<Inicio/>}/>
-          <Route path="/login" element={<Logins/>}/>
-
+          <Route index element={<Inicio />} />
+          <Route path="/login" element={<Logins />} />
         </Routes>
-
-
       </BrowserRouter>
-      
+      <PoolRepartidor />
     </div>
   );
 }
